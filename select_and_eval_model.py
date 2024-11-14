a39facdf107c9da6c8c7753fcf8ec45cdf64d26b
#JOSHUA ADRIAN CAHYONO# <jo0001no@CCDS-TC1.cm.cluster>
2024-11-15 02:15:51 +0800

    fixes

--- select_and_eval_model.py
+++ select_and_eval_model.py
@@ -10,18 +10,32 @@ def create_bleu_plot(best_model_metrics):
         # create subplot for greedy bleu and beam bleu
         f, ax = plt.subplots(1, 2, figsize=(15, 5))
         for model in best_model_metrics[dataset]:
+            # # modify such that the epoch is of multple of 5
             metrics = best_model_metrics[dataset][model]
-            ax[0].plot(metrics['val_greedy_bleus'], label=model)
+            metrics = best_model_metrics[dataset][model]
+
+            # Generate x-tick labels as multiples of 5, but use normal indexing for plotting
+            x_indices = list(range(len(metrics['val_greedy_bleus'])))
+            x_labels = [(i + 1) * 5 for i in x_indices]
+
+            # Plot Greedy BLEU
+            ax[0].plot(x_indices, metrics['val_greedy_bleus'], label=model)
             ax[0].set_title(f"Greedy BLEU for {dataset}")
             ax[0].set_xlabel("Epoch")
             ax[0].set_ylabel("BLEU Score")
             ax[0].legend()
-            
-            ax[1].plot(metrics['val_beam_bleus'], label=model)
+            ax[0].set_xticks(x_indices)
+            ax[0].set_xticklabels(x_labels)  # Set custom labels as multiples of 5
+
+            # Plot Beam BLEU
+            ax[1].plot(x_indices, metrics['val_beam_bleus'], label=model)
             ax[1].set_title(f"Beam BLEU for {dataset}")
             ax[1].set_xlabel("Epoch")
             ax[1].set_ylabel("BLEU Score")
             ax[1].legend()
+            ax[1].set_xticks(x_indices)
+            ax[1].set_xticklabels(x_labels)  # Set custom labels as multiples of 5
+
         
         # check parent folder
         Path(f"./eval/bleu_plots/").mkdir(parents=True, exist_ok=True)
@@ -50,8 +64,6 @@ for model in model_list:
             train_model_path = f"./metric_logs/{model}/{dataset}/{trained_model}/train_val_to_epoch_{num_epoch_eval}.json"
             with open(train_model_path, 'r') as json_file:
                 train_data = json.load(json_file)
-                
-            print(train_data["val_beam_bleus"][-1], trained_model)
 
             if train_data["val_beam_bleus"][-1] > best_model_score:
                 best_model_score = train_data["val_beam_bleus"][-1]
